81b5b0b3a4f38719608166f535edda39c59817a2
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-12-06 20:18:16 +0100

    fix prettier issues

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { 
-  FastifyAdapter, 
-  NestFastifyApplication 
-} from '@nestjs/platform-fastify'
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule, 
-    new FastifyAdapter()
+    AppModule,
+    new FastifyAdapter(),
   );
   app.enableCors();
   await app.listen(3005);
